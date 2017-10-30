b5e429e0b43f2d0866852cc01e8804fb57664f0e
Kirill <k.churikov.work@gmail.com>
2017-10-30 13:31:01 +0300

    update lib

--- source/js/app.js
+++ source/js/app.js
@@ -1,7 +1,8 @@
-(function() {
-  'use strict';
+(function () {
+    'use strict';
 
-  setTimeout(function() {
-    document.querySelector('.greating_picture').classList.add('m--show');
-  }, 1000);
+    setTimeout(function () {
+        document.querySelector('.greating_picture').classList.add('m--show');
+    }, 1000);
+
 })();
